52dede93eb371cb94c09b120352c9562914b0db9
MirudhulaaMakto <144311152+MirudhulaaMakto@users.noreply.github.com>
2023-10-26 19:11:44 +0530

    Update BuilderPageWrappe.tsx

--- src/app/pages/Inventory/BuilderPageWrappe.tsx
+++ src/app/pages/Inventory/BuilderPageWrappe.tsx
@@ -1,28 +1,14 @@
 import React, {FC} from 'react'
 import {PageTitle} from '../../../_metronic/layout/core'
 import {Projects} from '../Inventory/products'
-<<<<<<< HEAD
-
-
-const BuilderPageWrapper: FC = () => {
-=======
   
 const BuilderPageWrapp: FC = () => {
->>>>>>> mirudhulaa
   return (
     <>
       <PageTitle breadcrumbs={[]}>Inventory</PageTitle>
       <Projects className='mb-5 mb-xl-8' />
-<<<<<<< HEAD
-
-=======
->>>>>>> mirudhulaa
     </>
   )
 }
 
-<<<<<<< HEAD
-export default BuilderPageWrapper
-=======
 export default BuilderPageWrapp;
->>>>>>> mirudhulaa

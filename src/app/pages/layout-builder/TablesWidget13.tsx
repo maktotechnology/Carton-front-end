cdcdf258c87449ca5204cebaed69a37aa6224d9b
MirudhulaaMakto <144311152+MirudhulaaMakto@users.noreply.github.com>
2023-12-15 22:17:19 +0530

    Update TablesWidget13.tsx

--- src/app/pages/layout-builder/TablesWidget13.tsx
+++ src/app/pages/layout-builder/TablesWidget13.tsx
@@ -3,8 +3,9 @@
 import {KTIcon} from '../../../_metronic/helpers'
 import React, { useEffect, useState, } from 'react';
 import './BuilderPage.css';
-import { Link, Route, Routes, useLocation, } from 'react-router-dom';
+import { Link, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
 import AddUserPage from './AddUserPage';
+
 
 type Props = {
   className: string;
@@ -36,8 +37,8 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
     const storedData = localStorage.getItem('productsData');
     const initialData = storedData ? JSON.parse(storedData) : [
       { 
-        Ref_ID: Date.now(), 
         Prod_Id: Date.now(),
+        Ref_Id: Date.now(), 
         Request_risedby: 'John Doe', 
         Transfer_Type: 'Internal', 
         Branch: 'CBE', 
@@ -46,7 +47,6 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
         Dated: new Date('2023-01-10'),
         Uom: '2',
         Quantity: 5,
-        //Status: 'Approved'
       }
      ];
     // Remove duplicates based on Prod_Id and store in local storage
@@ -83,8 +83,8 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const newFormData = {
-      Ref_ID: searchParams.get('Ref_ID') || '',
       Prod_Id: searchParams.get('Prod_Id') || '',
+      Ref_Id: searchParams.get('Ref_Id') || '',
       Request_risedby: searchParams.get('Request_risedby') || '',
       Transfer_type: searchParams.get('Transfer_type') || '',
       Branch: searchParams.get('Branch') || '',
@@ -98,8 +98,8 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
 
     // Add the new user data to the sampleData array if all required fields are present
     if (
-      newFormData.Ref_ID &&
       newFormData.Prod_Id &&
+      newFormData.Ref_Id &&
       newFormData.Request_risedby &&
       newFormData.Transfer_type &&
       newFormData.Branch &&
@@ -112,23 +112,39 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
     ) {
       handleUserAdded(newFormData);
     }
-    
   }, [location.search]);
   
   // function to delete row
-  const handleDeleteRow = (prodId) => {
+  const handleDeleteRow = (Ref_Id) => {
     console.log('handleDeleteRow is called');
     // Filter out the row to be deleted from the data state
-    const updatedData = sampleData.filter((item) => item.Prod_Id !== prodId);
+    const updatedData = sampleData.filter((item) => item.Ref_Id !== Ref_Id);
     // Update the state first
     setSampleData(updatedData);
     console.log('UpdatedData after handleDeleteRow', updatedData)
     // Update the local storage with the filtered data
     localStorage.setItem('productsData', JSON.stringify(updatedData));
   };
-  
-  
+
+  // Create a dictionary to store rows based on Ref_Id
+  const rowsByRefId = sampleData.reduce((acc, data) => {
+    if (!acc[data.Ref_Id]) {
+      acc[data.Ref_Id] = [];
+    }
+    acc[data.Ref_Id].push(data);
+    return acc;
+  }, {});
+
+  console.log('row:', rowsByRefId);
+
+  // Assuming rowsByRefId is an object with keys being Ref_Ids
+  const firstRefId = Object.keys(rowsByRefId)[1];
+  const firstRowData = rowsByRefId[firstRefId];
+
+  console.log('First row data:', firstRowData);
+
   return (
+    // begin::div
     <div className={`card ${className}`}>
       {/* begin::Header */}
       <div className='card-header border-0 pt-5'>
@@ -180,62 +196,102 @@ const TablesWidget13: React.FC<Props> = ({ className }) => {
                 <th className='min-w-120px'>Transfer_type</th>
                 <th className='min-w-120px'>Branch</th>
                 <th className='min-w-120px'>Department</th>
-                <th className='min-w-120px'>Product</th>
-                <th className='min-w-120px'>Date</th>
-                <th className='min-w-120px'>UOM</th>
-                <th className='min-w-120px'>Quantity</th>
-                {/* <th className='min-w-120px'>Status</th> */}
+                <th className='min-w-120px'>Status</th>
                 <th className='min-w-100px text-end'>Actions</th>
               </tr>
             {/* end::Table head */}
             </thead>
-            {/* begin::Table body */}
+
             <tbody>
-            {sampleData.map((data, index) => (
-              <tr key={data.Prod_Id}>
+            {Object.entries(rowsByRefId).map(([refId, rows], index) => (
+              <tr key={index}>
                 <td>
-                  <div className='form-check form-check-sm form-check-custom form-check-solid'>
-                    <input className='form-check-input widget-13-check' type='checkbox' value='1' />
-                  </div>
-                </td>
+                    <div className='form-check form-check-sm form-check-custom form-check-solid'>
+                      {/* Insert Checkbox */}
+                      <input
+                        className='form-check-input widget-13-check'
+                        type='checkbox'
+                        value='1'
+                      />
+                    </div>
+                  </td>
+                {/* Render your columns for the Ref_Id (e.g., Ref_ID, Request_risedby, etc.) */}
                 <td>{index + 1}</td>
                 <td>
-                  {/* Make the name a clickable link and pass the user's name as a URL parameter */}
-                  <Link to={`/user-details/${encodeURIComponent(data.Ref_ID)}/${encodeURIComponent(data.Prod_Id)}/${encodeURIComponent(data.Request_risedby)}/${encodeURIComponent(data.Transfer_type)}/${encodeURIComponent(data.Branch)}/${encodeURIComponent(data.Department)}/${encodeURIComponent(data.Product)}/${encodeURIComponent(data.Dated)}/${encodeURIComponent(data.Uom)}/${encodeURIComponent(data.Quantity)}`}>
-                    {data.Request_risedby}
+                  <Link
+                    to={`/user-details/${
+                      encodeURIComponent((rows as any[])[0].Prod_Id)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Ref_Id)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Request_risedby)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Transfer_type)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Branch)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Department)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Product)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Dated)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Uom)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Quantity)
+                    }`} >
+                    {(rows as any[])[0].Request_risedby}
                   </Link>
                 </td>
-                <td>{data.Transfer_type}</td>
-                <td>{data.Branch}</td>
-                <td>{data.Department}</td>
-                <td>{data.Product}</td>
-                <td>{data.Dated}</td>
-                <td>{data.Uom}</td>
-                <td>{data.Quantity}</td>
-                {/* <td>{data.Status}</td> */}
+                <td>{(rows as any)[0].Transfer_type}</td>
+                <td>{(rows as any)[0].Branch}</td>
+                <td>{(rows as any)[0].Department}</td>
+                <td>Draft</td>
                 <td className='text-end'>
                   {/* Make the button(edit) a clickable link and pass the URL parameter */}
-                  <Link to={`/user-details/${encodeURIComponent(data.Ref_ID)}/${encodeURIComponent(data.Prod_Id)}/${encodeURIComponent(data.Request_risedby)}/${encodeURIComponent(data.Transfer_type)}/${encodeURIComponent(data.Branch)}/${encodeURIComponent(data.Department)}/${encodeURIComponent(data.Product)}/${encodeURIComponent(data.Dated)}/${encodeURIComponent(data.Uom)}/${encodeURIComponent(data.Quantity)}}`}>
-                    <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'>
+                  <Link
+                    to={`/user-details/${
+                      encodeURIComponent((rows as any[])[0].Prod_Id)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Ref_Id)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Request_risedby)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Transfer_type)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Branch)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Department)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Product)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Dated)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Uom)
+                    }/${
+                      encodeURIComponent((rows as any[])[0].Quantity)
+                    }?editMode=true`} >
+                    <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1' >
                       <KTIcon iconName='pencil' className='fs-3' />
                     </a>
                   </Link>
                   {/* Make the button clickable to delete the row */}
                   <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'
-                    onClick={() => handleDeleteRow(data.Prod_Id)}>
-                      <KTIcon iconName='trash' className='fs-3' />
+                    onClick={() => handleDeleteRow((rows as any[])[0].Ref_Id)}>
+                    <KTIcon iconName='trash' className='fs-3' />
                   </a>
                 </td>
               </tr>
             ))}
-            {/* end::Table body */}
-            </tbody>
+          </tbody>
+
           {/* end::Table */}
           </table>
         {/* end::Table container */}
         </div>
       {/* begin::Body */}
       </div>
+    {/* end::div */}
     </div> 
   )
 }

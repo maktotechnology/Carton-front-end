cb8921a3c1be6d5e379f547327b72c0e94bac01a
vinothrajmakto <vinoth@maktotechnology.com>
2024-07-08 10:45:56 +0530

    User Management

---
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -0,0 +1,22 @@
+
+import {useIntl} from 'react-intl'
+import {KTIcon} from '../../../helpers'
+import {AsideMenuItemWithSub} from './AsideMenuItemWithSub'
+import {AsideMenuItem} from './AsideMenuItem'
+
+export function AsideMenuMain() {
+  const intl = useIntl()
+
+  return (
+    <>
+      <AsideMenuItem
+        to='/dashboard'
+        icon='element-11'
+        title={intl.formatMessage({id: 'MENU.DASHBOARD'})}
+      />
+      <AsideMenuItem to='/apps/user-management/users' icon='shield-tick' title='User management' />
+ 
+
+    </>
+  )
+}

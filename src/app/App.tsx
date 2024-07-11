efbf455208a57466915a26b2999d60f68e2f6ac0
vinothrajmakto <vinoth@maktotechnology.com>
2024-07-11 12:36:53 +0530

    Chatwoot

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,10 +5,55 @@ import {LayoutProvider, LayoutSplashScreen} from '../_metronic/layout/core'
 import {MasterInit} from '../_metronic/layout/MasterInit'
 import {AuthInit} from './modules/auth'
 import {ThemeModeProvider} from '../_metronic/partials'
+import {useEffect} from 'react'
+
+
+// Extend the Window interface to include chatwootSDK
+declare global {
+  interface Window {
+    chatwootSDK: {
+      run: (config: { websiteToken: string, baseUrl: string }) => void
+    }
+  }
+}
 
 const App = () => {
+
+  useEffect(() => {
+    
+    const script = document.createElement('script')
+
+    
+    script.src = "http://13.234.34.123:3000/packs/js/sdk.js"
+    script.defer = true
+    script.async = true
+    document.body.appendChild(script)
+
+    script.onload = () => {
+      console.log('Chatwoot script loaded')
+      if (window.chatwootSDK) {
+        window.chatwootSDK.run({
+          websiteToken: 'wMgCkYYSLjSGsRE8mwFk25VV',
+          baseUrl: "http://13.234.34.123:3000/"
+        })
+        console.log('Chatwoot initialized')
+      } else {
+        console.error('Chatwoot SDK not available on window object')
+      }
+    }
+
+    script.onerror = () => {
+      console.error('Error loading Chatwoot script')
+    }
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
   return (
+    
     <Suspense fallback={<LayoutSplashScreen />}>
+      
       <I18nProvider>
         <LayoutProvider>
           <ThemeModeProvider>

ff3924c9ea2c94c7868cadc0dfe5b59ae8544c24
vinothrajmakto <vinoth@maktotechnology.com>
2024-10-02 10:12:43 +0530

    Customer Management

--- src/app/modules/apps/customer-management/CustomerProfile/components/settings/Settings.tsx
+++ src/app/modules/apps/customer-management/CustomerProfile/components/settings/Settings.tsx
@@ -5,11 +5,7 @@ export function Settings() {
   return (
     <>
       <ProfileDetails />
-<<<<<<< HEAD
 
-=======
-      <DeactivateAccount />
->>>>>>> 7af00896a516255c5b4fca3789fb0d10d0737ef9
     </>
   )
 }
